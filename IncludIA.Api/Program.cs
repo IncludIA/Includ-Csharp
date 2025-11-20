b34c81f894a7d1093176f2961b53711c729870bf
Lu1zEdu <le6269199@gmail.com>
2025-11-20 18:27:32 -0300

    Program pronto eu acho

--- IncludIA.Api/Program.cs
+++ IncludIA.Api/Program.cs
@@ -169,7 +169,11 @@ app.UseSerilogRequestLogging();
 
 // Swagger habilitado apenas em desenvolvimento (ou mude conforme necessidade)
 app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IncludIA API v1"));
+app.UseSwaggerUI(c => 
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "IncludIA API v1");
+    c.RoutePrefix = string.Empty; 
+});
 
 app.UseHttpsRedirection();
 
